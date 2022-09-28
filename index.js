d303e4e3556c2e7bbe8e22bcfe55c4f7e8ccd166
Pierreabc <pierre.baraban34@gmail.com>
2022-09-28 14:52:24 +0200

    init

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 const Element1 = document.getElementById('conteneur');
 console.log(Element1);
+
+const newElt = document.createElement("div");
+let elt = document.getElementById("conteneur");
+
+elt.appendChild(newElt);
+
+const newElt2 = document.createElement("div");
+elt.appendChild(newElt2);
+
+let elt = document.getElementById('conteneur');
+elt.innerHTML = "<ul><li>Element1</li><li>Element2</li></ul>"
